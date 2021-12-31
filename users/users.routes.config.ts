f25f177b06d9fe33ccaa6dd517cc76dee4d647e2
Viola Viola <hustlaviola@gmail.com>
2021-12-31 19:54:30 +0100

    Set up project

---
+++ users/users.routes.config.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import { CommonRoutesConfig } from '../common/common.routes.config';
+
+export class UsersRoutes extends CommonRoutesConfig {
+    constructor(app: express.Application) {
+        super(app, 'UsersRoutes');
+    }
+    
+    configureRoutes() {
+        return this.app;
+    }
+}
